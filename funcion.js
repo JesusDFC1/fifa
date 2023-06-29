74ab4b732fc614c6013753f8246aebf3ec9a6ab2
AndrewGremory <Andresamayap12313@gmail.com>
2023-06-29 09:46:23 -0500

    segundo commit

---
+++ funcion.js
@@ -0,0 +1,33 @@
+// Ejemplo de animación
+const header = document.querySelector('header');
+header.addEventListener('click', () => {
+  header.style.backgroundColor = 'blue';
+});
+function tico() {
+  document.getElementById('pie').innerHTML= ""
+  +"<div><ul><li>Estadio Arena Castelão</li></ul></div><br>"
+  +"<div><ul><li>El recinto fue inaugurado el 11 de noviembre de 1973 y posee una capacidad máxima para 65 000 espectadores sentados tras su última remodelación en 2013</li></ul></div> ";
+}
+function tica() {
+  document.getElementById('pio').innerHTML= ""
+  +"<div><ul><li>Estadio Beira-Rio</li></ul></div><br>"
+  +"conocido también como José Pinheiro Borda es un estadio de fútbol brasileño ubicado en la ciudad de Porto Alegre, en la costa del río Guaiba, estado de Rio Grande do Sul. El equipo al que pertenece es el Internacional";
+}
+function ticy() {
+  document.getElementById('piu').innerHTML= ""
+  +"Estadio Mineirão <br> <br>"
+  +"Inaugurado en 1965, el Mineirão tiene capacidad actual para 60 000 espectadores. Sufrió una importante reducción en su capacidad para poder acogerse a las normas de la FIFA para los estadios de fútbol.";
+}
+function ticu() {
+  document.getElementById('pia').innerHTML= ""
+  +"Estadio de Maracaná <br> <br>"
+  +"Es el estadio más grande de Brasil y fue el más grande del mundo durante mucho tiempo. Inició su construcción a principios de 1949 como parte de los esfuerzos locales por obtener la sede de la Copa Mundial de Fútbol";
+}
+function ticb() {
+  document.getElementById('pik').innerHTML= ""
+  +"Estadio de brasilia <br> <br>"
+  +" es un estadio multiusos ubicado en Brasília en el Distrito Federal en Brasil. Fue inaugurado en 1974 y tras la última remodelación, de 2013, cuenta con una capacidad de 72 800 espectadores sentados.";
+}
+
+
+
